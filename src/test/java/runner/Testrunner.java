b3af1330f0bff5a93bb2d751f38af500acac911d
Ingeniero21 <ingeniero21696@gmail.com>
2022-09-04 21:02:16 -0500

    otra prueba

--- src/test/java/runner/Testrunner.java
+++ src/test/java/runner/Testrunner.java
@@ -3,7 +3,7 @@ package runner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
-
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
